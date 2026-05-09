ea9f7f3dc649ed19ce78d44ec3f70a6541e70ab1
Andrew Zhang <23qh42@queensu.ca>
2026-05-08 23:26:50 -0400

    Draw stuff I guess

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ pygame.init()
 
 
 def create_window():
-    window = pygame.display.set_mode((800, 600), pygame.RESIZABLE, vsync=True)
+    window = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, vsync=True)
     return window
 
 def game_loop(window):
+    pygame.display.set_caption("The Goat")
     running = True
     while running:
         # 1. Event handling
@@ -19,14 +20,22 @@ def game_loop(window):
                 running = False
 
 
-
+        window.fill((155, 69, 0))   # clear screen
         # 2. Update
         # (game logic goes here)
+        pygame.draw.rect(window, (255, 255, 255), (0, 0, 67, 69), 2)
+        pygame.draw.circle(window, (67, 255, 255), (100, 100), 34)
+        pygame.draw.ellipse(window, (255, 255, 255), (200, 200, 100, 50), 2)
+        pygame.draw.polygon(window, (255, 255, 255), [(720, 720), (690, 634), (720, 790)])
+        pygame.draw.line(window, (255, 255, 255), (1080, 720), (555, 555), 2)
+        pygame.draw.lines(window, (255, 255, 255), False, [(240, 10), (260, 55), (320, 22)], 2)
+        pygame.draw.arc(window, (255, 255, 255), (100, 100, 100, 50), 0, math.pi/2, 2)
 
+    
 
 
         # 3. Render
-        window.fill((155, 69, 0))   # clear screen
+
         # (draw calls go here)
         pygame.display.update()
 
