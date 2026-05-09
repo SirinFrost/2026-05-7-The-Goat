fe01c40fdfd4b683af732730f36f18224eb926a4
Andrew Zhang <23qh42@queensu.ca>
2026-05-09 12:49:09 -0400

    Cube Player Moving

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import math
 
+from player import Player
+
 
 pygame.init()
 
@@ -13,23 +15,34 @@ def create_window():
 def game_loop(window):
     pygame.display.set_caption("The Goat")
     running = True
+
+    player = Player(680, 360, 100, 100)
     while running:
         # 1. Event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            player.x -= player.speed
+        if keys[pygame.K_d]:
+            player.x += player.speed
+        if keys[pygame.K_w]:
+            player.y -= player.speed
+        if keys[pygame.K_s]:
+            player.y += player.speed
+
+        player.rect.x = player.x
+        player.rect.y = player.y
 
         window.fill((155, 69, 0))   # clear screen
         # 2. Update
         # (game logic goes here)
-        pygame.draw.rect(window, (255, 255, 255), (0, 0, 67, 69), 2)
-        pygame.draw.circle(window, (67, 255, 255), (100, 100), 34)
-        pygame.draw.ellipse(window, (255, 255, 255), (200, 200, 100, 50), 2)
-        pygame.draw.polygon(window, (255, 255, 255), [(720, 720), (690, 634), (720, 790)])
-        pygame.draw.line(window, (255, 255, 255), (1080, 720), (555, 555), 2)
-        pygame.draw.lines(window, (255, 255, 255), False, [(240, 10), (260, 55), (320, 22)], 2)
-        pygame.draw.arc(window, (255, 255, 255), (100, 100, 100, 50), 0, math.pi/2, 2)
+
+        pygame.draw.rect(window, player.color, player.rect)
+        
+        # Draw shapes
 
     
 
---
+++ player.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Player:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = (124, 69, 67)
+        self.speed = 5
